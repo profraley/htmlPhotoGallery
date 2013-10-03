74c7f6e1f7036bdc61e2efe014efe0dd1954864a
Chris Raley <craley@smu.edu>
2013-10-03 13:39:30 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+The initial start of a functioning Photo Gallery. 
+
+Some HTML, CSS, and Javascript have already been created. 
+Using jQuery position elements to make the application look like the finished_gallery.png.
---
+++ js/gallery.js
@@ -0,0 +1,59 @@
+// requestAnim shim layer by Paul Irish
+    window.requestAnimFrame = (function(){
+      return  window.requestAnimationFrame       || 
+              window.webkitRequestAnimationFrame || 
+              window.mozRequestAnimationFrame    || 
+              window.oRequestAnimationFrame      || 
+              window.msRequestAnimationFrame     || 
+              function(/* function */ callback, /* DOMElement */ element){
+                window.setTimeout(callback, 1000 / 60);
+              };
+    })();
+  
+
+// example code from mr doob : http://mrdoob.com/lab/javascript/requestanimationframe/
+
+animate();
+
+var mLastFrameTime = 0;
+var mWaitTime = 5000; //time in ms
+function animate() {
+    requestAnimFrame( animate );
+	var currentTime = new Date().getTime();
+	if (mLastFrameTime === 0) {
+		mLastFrameTime = currentTime;
+	}
+
+	if ((currentTime - mLastFrameTime) > mWaitTime) {
+		swapPhoto();
+		mLastFrameTime = currentTime;
+	}
+}
+
+/************* DO NOT TOUCH CODE ABOVE THIS LINE ***************/
+
+function swapPhoto() {
+	//Add code here to access the #slideShow element.
+	//Access the child img element and replace its source
+	//with a new image from your images array which is loaded 
+	//from the JSON string
+	console.log('swap photo');
+}
+
+
+var mCurrentIndex = 0;
+var request = new XMLHttpRequest();
+var mImages = new Array();
+var json;
+var url = 'insert_url_here_to_image_json';
+
+
+$(document).ready( function() {
+	$('.details').eq(0).hide();
+});
+
+window.addEventListener('load', function() {
+	console.log('window loaded');
+}, false);
+
+
