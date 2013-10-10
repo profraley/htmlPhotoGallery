d56db4ea71a93369ffdb24dce96c3193a2bc2757
Chris Raley <craley@smu.edu>
2013-10-09 19:27:14 -0500

    Ignore previous commit. Added a helper method to deal with javascript closure craziness. Use the makeGalleryImageOnloadCallback method as your image's onload event listener.

--- js/gallery.js
+++ js/gallery.js
@@ -43,10 +43,47 @@ function swapPhoto() {
 
 var mCurrentIndex = 0;
 var request = new XMLHttpRequest();
-var mImages = new Array();
+
+//Use this array to hold objects which contain the following:
+//location, description, date and an actual Image element.
+var mImages = [];
 var json;
 var url = 'insert_url_here_to_image_json';
 
+
+//This is probably seems confusing, but in javascript there is a concept known as closures. Closures allows
+//variables to remain in scope after a method finishes executing. You can read about it here: http://stackoverflow.com/a/2803496
+//All you need to know is that you should use the following function as your event callback for loading the source of Images from your json data.
+
+//@param A GalleryImage object. Use this method for an event handler for loading a gallery Image object.
+function makeGalleryImageOnloadCallback(galleryImage) {
+	return function(e) {
+		galleryImage.img = e.target;
+		mImages.push(galleryImage);
+	}
+}
+
+function testClosureExample() {
+	for (var i = 0; i < 4; i++) {
+		var input = document.createElement("input");
+		input.type = "button";
+		input.value = "Button " + i;
+		//trying to access the value i like the click listener is doing will lead to bad results.
+		input.onclick = function(e) {
+			console.log("The variable i will always equal 4. i = " + i);
+		}
+		
+		//using a closure to fight a closure gives us what we would expect.
+		input.onmouseup = testClosureExampleMouseUpHandler(i);
+		document.body.appendChild(input);
+	}
+}
+
+function testClosureExampleMouseUpHandler(i) {
+	return function(e) {
+		console.log("The variable is will be what you expect, and match the value the button has. i = " + i);
+	}
+}
 
 $(document).ready( function() {
 	$('.details').eq(0).hide();
@@ -54,6 +91,7 @@ $(document).ready( function() {
 
 window.addEventListener('load', function() {
 	console.log('window loaded');
+	testClosureExample();
 }, false);
 
 
